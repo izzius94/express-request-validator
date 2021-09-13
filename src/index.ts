2e7ff4051c8bce8a125488337d5ec01f8d128e18
Maurizio Urso <maurizio.urso@outlook.it>
2021-09-13 02:21:25 +0200

    fixing naming

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export {default as middleware, IRequest} from './lib/middleware';
 export {default as HttpRequest} from './lib/HttpRequest';
-export {Exception as ValidationExcpetion} from './lib/Exception';
+export {Exception as ValidationException} from './lib/Exception';
